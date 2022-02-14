c77101b57e0630c18f14cd5fc538afbe9d7d1a80
hugokelven <hugokelven98@gmail.com>
2022-02-14 01:09:32 -0300

    participantes ativos

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function usuarioCadastrado(resposta) {
     console.log(resposta) //retirar depois
 }
 
+// executa uma série de ações em caso de falha ao cadastrar o usuario
 function usuarioNaoCadastrado(erro) {
     console.log(erro) //retirar depois
     const fundoEscuro = document.querySelector(".fundo-escuro")
@@ -27,7 +28,7 @@ function usuarioNaoCadastrado(erro) {
     fundoEscuro.classList.remove("escondido")
     error400.classList.remove("escondido")
 
-    setTimeout(() => {document.location.reload(true)}, 5000)
+    setTimeout(() => {window.location.reload()}, 5000)
 }
 
 setInterval(manterConexaoDoUsuario, 5000)
@@ -54,17 +55,17 @@ function buscarMensagens(mensagens) {
 
         if (mensagens.data[i].type === "status") {
             chat.innerHTML += `
-            <span class="mensagem__usuario-entrou"><p>(${mensagens.data[i].time}) <strong>&nbsp${mensagens.data[i].from}&nbsp</strong>  ${mensagens.data[i].text}</p></span>
+            <span data-identifier="message" class="mensagem__usuario-entrou"><p>(${mensagens.data[i].time}) <strong>&nbsp${mensagens.data[i].from}&nbsp</strong>  ${mensagens.data[i].text}</p></span>
             `
         } else if (mensagens.data[i].type === "message") {
             chat.innerHTML += `
-            <span class="mensagem"><p>(${mensagens.data[i].time}) <strong>&nbsp${mensagens.data[i].from}&nbsp</strong> para <strong>&nbsp${mensagens.data[i].to}</strong>: ${mensagens.data[i].text}</p></span>
+            <span data-identifier="message" class="mensagem"><p>(${mensagens.data[i].time}) <strong>&nbsp${mensagens.data[i].from}&nbsp</strong> para <strong>&nbsp${mensagens.data[i].to}</strong>: ${mensagens.data[i].text}</p></span>
             `
-        } else {
+        } else if (mensagens.data[i].to === nomeUsuario || mensagens.data[i].from === nomeUsuario) {
             chat.innerHTML += `
-            <span class="mensagem__privada"><p>(${mensagens.data[i].time}) <strong>&nbsp${mensagens.data[i].from}&nbsp</strong> reservadamente para <strong>&nbsp${mensagens.data[i].to}</strong>: ${mensagens.data[i].text}</p></span>
+            <span data-identifier="message" class="mensagem__privada"><p>(${mensagens.data[i].time}) <strong>&nbsp${mensagens.data[i].from}&nbsp</strong> reservadamente para <strong>&nbsp${mensagens.data[i].to}</strong>: ${mensagens.data[i].text}</p></span>
             `
-        }
+        } else { }
 
         scrollarAteUltimaMensagem()
     }
@@ -76,11 +77,20 @@ function scrollarAteUltimaMensagem() {
 }
 
 function enviarMensagem() {
+    const publico = document.querySelector("aside span")
+    console.log(publico)
+
+    if (publico.classList.contains("selecionado")) {
+        privacidadeDaMensagem = "message"
+    } else {
+        privacidadeDaMensagem = "private_message"
+    }
+
     mensagem = {
         from: nomeUsuario,
-        to: "Todos",
+        to: contatoSelecionado,
         text: document.querySelector("footer input").value,
-        type: "message"
+        type: privacidadeDaMensagem
     }
 
     const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', mensagem)
@@ -89,11 +99,126 @@ function enviarMensagem() {
     promessa.catch(mensagemNaoenviada)
 }
 
+// executa uma ação em caso de sucesso no envio da mensagem
 function mensagemEnviada(resposta) {
     console.log(resposta)
+    document.querySelector("footer input").value = ""
     mostrarMensagens()
 }
 
+// executa uma ação em caso de falha no envio da mensagem
 function mensagemNaoenviada(erro) {
     console.log(erro)
+    window.location.reload()
+}
+
+function habilitarBarraLateral() {
+    const fundoEscuro = document.querySelector(".fundo-escuro")
+    const barraLateral = document.querySelector("aside")
+
+    fundoEscuro.classList.remove("escondido")
+    barraLateral.classList.remove("escondido")
+}
+
+function voltarAoChat() {
+    const fundoEscuro = document.querySelector(".fundo-escuro")
+    const barraLateral = document.querySelector("aside")
+
+    fundoEscuro.classList.add("escondido")
+    barraLateral.classList.add("escondido")
+}
+
+mostrarParticipantes()
+
+setInterval(mostrarParticipantes, 10000)
+
+function mostrarParticipantes() {
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/participants')
+
+    promessa.then(carregarParticipantes)
+}
+
+//caso o contato selecionado saia da sala, a opção "Todos" é marcada automaticamente
+function verificarConexaoDoContatoSelecionado(contatos) {
+    let count = 0
+    for (let i = 0; i < contatos.data.length; i++) {
+        if (contatoSelecionado === contatos.data[i].name) {
+            count++
+        } else { }
+    }
+    if (count === 0 && contatoSelecionado !== "Todos") {
+        document.querySelector("footer input").value = ""
+
+        const aviso = document.querySelector(".usuario-saiu")
+
+        aviso.innerHTML = `
+        Usuário <strong>${contatoSelecionado}</strong> deixou a sala.
+        `
+        aviso.classList.remove("escondido")
+        setTimeout(() => {aviso.classList.add("escondido")}, 3000)
+
+        contatoSelecionado = "Todos"
+    } else { }
+}
+
+function carregarParticipantes(participantes) {
+    console.log(participantes.data) //apagar depois
+    const contatos = document.querySelector(".contatos")
+
+    verificarConexaoDoContatoSelecionado(participantes)
+
+    if (contatoSelecionado === "Todos") {
+        contatos.innerHTML = `
+    <div data-identifier="participant" class="selecionado">
+        <img src="icons/persons.svg" alt="pessoas">
+        <p>Todos</p>
+    </div>
+    `
+    } else {
+        contatos.innerHTML = `
+    <div data-identifier="participant" onclick="selecionarParticipante(this)">
+        <img src="icons/persons.svg" alt="pessoas">
+        <p>Todos</p>
+    </div>
+    `
+    }
+
+
+    for (let i = 0; i < participantes.data.length; i++) {
+        if (participantes.data[i].name === contatoSelecionado) {
+            contatos.innerHTML += `
+            <div data-identifier="participant" class="selecionado" onclick="selecionarParticipante(this)">
+                <img src="icons/user.svg" alt="usuário">
+                <p>${participantes.data[i].name}</p>
+            </div>
+        `
+        } else {
+            contatos.innerHTML += `
+            <div data-identifier="participant" onclick="selecionarParticipante(this)">
+                <img src="icons/user.svg" alt="usuário">
+                <p>${participantes.data[i].name}</p>
+            </div>
+        `
+        }
+    }
+}
+
+let contatoSelecionado = "Todos"
+
+function selecionarParticipante(contato) {
+    const participante = document.querySelector(".contatos .selecionado")
+    participante.classList.remove("selecionado")
+
+    contato.classList.add("selecionado")
+
+    let nomeDoContatoSelecionado = document.querySelector(".contatos .selecionado p")
+    contatoSelecionado = nomeDoContatoSelecionado.innerHTML
+    console.log(contatoSelecionado)
+}
+
+function selecionarVisibilidade(elemento) {
+    const visibilidade = document.querySelector("aside span.selecionado")
+    visibilidade.classList.remove("selecionado")
+
+    elemento.classList.add("selecionado")
 }
